b33423d2e365a83ed8b2a492d570a80c4b334715
Emre Karatas <85746327+emre-karatas@users.noreply.github.com>
2023-05-23 08:08:29 +0300

    update ruby

--- HW02/22001641_karatas_emre.rb
+++ HW02/22001641_karatas_emre.rb
@@ -1,31 +1,31 @@
 puts('---5.1---')
 # Type Checking Example
-def print_famous_cs_people_details(name, birth_year)
+def print_famous_cs_people(name, birth_year)
   puts "Name: #{name}, Birth Year: #{birth_year}"
 end
 
-print_famous_cs_people_details("Alan Turing", 1912)
-print_famous_cs_people_details(1912, "Alan Turing") # This also works but the output is not as expected.
+print_famous_cs_people("Alan Turing", 1912)
+print_famous_cs_people(1912, "Alan Turing") # This also works but the output is not as expected.
 
 puts('---5.2---')
 # Named Parameters Example
-def print_famous_cs_people_details(name:, birth_year:)
+def print_famous_cs_people_named_params(name:, birth_year:)
   puts "Name: #{name}, Birth Year: #{birth_year}"
 end
 
-print_famous_cs_people_details(birth_year: 1939, name: "Barbara Liskov")
+print_famous_cs_people_named_params(birth_year: 1939, name: "Barbara Liskov")
 
 puts('---5.3---')
 # Default Parameters Example
-def print_famous_cs_people_details(name = "Ada Lovelace", birth_year = 1815)
+def print_famous_cs_people_default_params(name = "Ada Lovelace", birth_year = 1815)
   puts "Name: #{name}, Birth Year: #{birth_year}"
 end
 
-print_famous_cs_people_details()
+print_famous_cs_people_default_params()
 
 puts('---5.4---')
 # Parameter Passing Methods Example
-def change_famous_cs_people_details(name, details)
+def change_famous_cs_people_param_passing(name, details)
   name = "Bjarne Stroustrup"
   details[:name] = "Bjarne Stroustrup"
 end
@@ -33,14 +33,14 @@ end
 name = "Elon Musk"
 details = {name: "Jeff Bezos"}
 
-change_famous_cs_people_details(name, details)
+change_famous_cs_people_param_passing(name, details)
 
 puts name  # prints "Elon Musk"
 puts details[:name]  # prints "Bjarne Stroustrup"
 
 puts('---5.5---')
 # Subprograms as Parameters Example
-def print_famous_cs_people_details(name, birth_year)
+def print_famous_cs_people_sub(name, birth_year)
   puts "Name: #{name}, Birth Year: #{birth_year}"
 end
 
@@ -51,7 +51,7 @@ def print_with_introduction(func, name, birth_year)
 end
 
 print_func = Proc.new do |name, birth_year|
-  print_famous_cs_people_details(name, birth_year)
+  print_famous_cs_people_sub(name, birth_year)
 end
 
 print_with_introduction(print_func, "Larry Page", 1973)
